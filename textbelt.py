1d20f3fd81a8def3fab43ce456fb815312d48ba7
Kaz Bishop <EvilLogic@users.noreply.github.com>
2017-07-08 02:26:09 -0700

    Added textbelt

---
+++ textbelt.py
@@ -0,0 +1,8 @@
+import requests
+requests.post('https://textbelt.com/text', {
+  'phone': 'text#redacted',
+  'message': 'Hello world',
+  'key': 'textbelt',
+})
+# only really sending
+# very limited windows support
